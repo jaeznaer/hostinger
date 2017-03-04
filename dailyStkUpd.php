4c1f2ca9470ba8fc75041cef9a36f85d0d4c7602
jaeznaer <jaeznaer11@gmail.com>
2017-03-04 17:36:30 +0530

    Update dailyStkUpd.php

--- dailyStkUpd.php
+++ dailyStkUpd.php
@@ -7,12 +7,12 @@ echo "Failed to connect to MySQL: " . mysqli_connect_error() ;
 $result = mysqli_query($con,"SELECT nse_stock_name FROM NseStocks");
 $stockArray = Array();
 while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {
-    $stocks[] =  $row['nse_stock_name'];  
+    $stockArray[] =  $row['nse_stock_name'];  
 }
 //$sql = "INSERT INTO UserDetails (SrNo, FirstName, LastName, Email, Password, Date) VALUES (NULL, 'jaez', 'naer', 'jayesh94nair@rediffmail.com', 'qwerty', '1994-02-09')";
 foreach ($stockArray as $stock){
 	echo $stock."<br>";
 }
 mysqli_close($con) ;
-echo "Connected to server closed successfully\r\n";
+echo "Connection to server closed successfully\r\n";
 ?>
