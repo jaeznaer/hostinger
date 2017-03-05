9160fad1b83f0008c534e3c33a2abe56f37a6a6f
jaeznaer <jaeznaer11@gmail.com>
2017-03-05 23:25:32 +0530

    Update currentStock.php

--- currentStock.php
+++ currentStock.php
@@ -3,6 +3,11 @@ $con=mysqli_connect("mysql.hostinger.in", "u659193285_jaez", "1994jitu", "u65919
 if (mysqli_connect_errno($con) ) {
 echo "Failed to connect to MySQL: " . mysqli_connect_error() ;
 }
+
+	//jaeznet.pe.hu/currentStock.php?nse-stock=NIFTY
+
+	$stock = $_GET[ 'nse-stock' ] ;
+
 	$json = file_get_contents("http://finance.google.com/finance/info?client=ig&q=NSE:$stock",NULL,NULL,4);
 	
 	print(json_decode($json));
