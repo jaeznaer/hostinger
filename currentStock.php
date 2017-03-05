1e4cc9345a2d5336b9ad3c4fa57f68164b6ae150
jaeznaer <jaeznaer11@gmail.com>
2017-03-05 23:21:36 +0530

    Add files via upload

---
+++ currentStock.php
@@ -0,0 +1,12 @@
+<?php
+$con=mysqli_connect("mysql.hostinger.in", "u659193285_jaez", "1994jitu", "u659193285_cldet") ;
+if (mysqli_connect_errno($con) ) {
+echo "Failed to connect to MySQL: " . mysqli_connect_error() ;
+}
+	$json = file_get_contents("http://finance.google.com/finance/info?client=ig&q=NSE:$stock",NULL,NULL,4);
+	
+	print(json_decode($json));
+
+mysqli_close($con) ;
+//echo "Connection to server closed successfully\r\n";
+?>
