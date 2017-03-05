499353b7729d8245df250459669f766be1e433d3
jaeznaer <jaeznaer11@gmail.com>
2017-03-05 10:26:40 +0530

    Add files via upload

---
+++ allStock.php
@@ -0,0 +1,19 @@
+<?php
+$con=mysqli_connect("mysql.hostinger.in", "u659193285_jaez", "1994jitu", "u659193285_cldet") ;
+if (mysqli_connect_errno($con) ) {
+echo "Failed to connect to MySQL: " . mysqli_connect_error() ;
+}
+
+$sql = "SELECT * FROM NseStocks";
+
+$result = mysqli_query($con,$sql);
+
+while ($row = mysqli_fetch_array($result))
+{
+		$data[] = $row;
+}
+
+print(json_encode($data));
+
+mysqli_close($con) ;
+?>		
---
+++ dayStock.php
@@ -0,0 +1,23 @@
+<?php
+$con=mysqli_connect("mysql.hostinger.in", "u659193285_jaez", "1994jitu", "u659193285_cldet") ;
+if (mysqli_connect_errno($con) ) {
+echo "Failed to connect to MySQL: " . mysqli_connect_error() ;
+}
+
+//jaeznet.pe.hu/dayStock.php?nse-stock=NIFTY
+
+$nse_stock = $_GET[ 'nse-stock' ] ; 
+
+$sql = "SELECT open, high, low, close, close_chg, close_cp, stamp FROM $nse_stock";
+
+$result = mysqli_query($con,$sql);
+
+while ($row = mysqli_fetch_array($result)) 
+{
+		$data[] = $row;
+}
+
+print(json_encode($data));
+
+mysqli_close($con) ;
+?>		
