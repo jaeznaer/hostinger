e00bcabc59c9a654cb1a5e592881019fb1aaad12
jaeznaer <jaeznaer11@gmail.com>
2017-03-06 17:02:35 +0530

    Update firstDiffValue.php

--- firstDiffValue.php
+++ firstDiffValue.php
@@ -15,58 +15,72 @@ foreach ($stockArray as $stock){
 }
 function fetchData($con, $stock){
 $stockArray = Array();
-	
+
+$sql = "SELECT stamp FROM Daily$stock
+ORDER BY id DESC
+LIMIT 1";
+$todayStamp = mysqli_query($con, $sql);
+$row = mysqli_fetch_array($todayStamp, MYSQL_ASSOC);
+$currentStamp=$row['stamp'];
+echo $currentStamp."<br>";
+
 $sql = "SELECT ltp FROM Daily$stock
+WHERE stamp = $currentStamp
 ORDER BY id ASC
 LIMIT 1";
 $open = mysqli_query($con, $sql);
 $row = mysqli_fetch_array($open, MYSQL_ASSOC);
 $stockArray[0]=$row['ltp'];
-//echo $stockArray[0]."<br>";
+echo $stockArray[0]."<br>";
+	
 $sql1 = "SELECT ltp FROM Daily$stock
+WHERE stamp = $currentStamp
 ORDER BY id DESC
 LIMIT 1";
 $close = mysqli_query($con, $sql1);
 $row = mysqli_fetch_array($close, MYSQL_ASSOC);
 $stockArray[1]=$row['ltp'];
-//	echo $stockArray[1]."<br>";
-$sql2 = "SELECT MAX(ltp) AS highPrice FROM Daily$stock";
+	echo $stockArray[1]."<br>";
+	
+$sql2 = "SELECT MAX(ltp) AS highPrice FROM Daily$stock WHERE stamp = $currentStamp";
 $high = mysqli_query($con, $sql2);
 //echo $high."<br>";
 //Object of class mysqli_result could not be converted to string
 //echo $high->fetch_object()->ltp;
 $row = mysqli_fetch_array($high, MYSQL_ASSOC);
 $stockArray[2]=$row['highPrice'];
-//	echo $stockArray[2]."<br>";
-$sql3 = "SELECT MIN(ltp) AS lowPrice FROM Daily$stock";
+	echo $stockArray[2]."<br>";
+	
+$sql3 = "SELECT MIN(ltp) AS lowPrice FROM Daily$stock WHERE stamp = $currentStamp";
 $low = mysqli_query($con, $sql3);
 //echo $low."<br>";
 $row = mysqli_fetch_array($low, MYSQL_ASSOC);
 $stockArray[3]=$row['lowPrice'];
-//	echo $stockArray[3]."<br>";
+	echo $stockArray[3]."<br>";
+	
 $closePrice = round($stockArray[1],3);
 	
 $sql4 = "SELECT DISTINCT chg FROM Daily$stock WHERE ROUND(ltp,3) = '$closePrice'";
 $close_chg = mysqli_query($con, $sql4);
 $row = mysqli_fetch_array($close_chg, MYSQL_ASSOC);
 $stockArray[4]=$row['chg'];
-//	echo $stockArray[4]."<br>";
+	echo $stockArray[4]."<br>";
 $sql5 = "SELECT DISTINCT chg_p FROM Daily$stock WHERE ROUND(ltp,3) = '$closePrice'";
 $close_cp = mysqli_query($con, $sql5);
 $row = mysqli_fetch_array($close_cp, MYSQL_ASSOC);
 $stockArray[5]=$row['chg_p'];
-//	echo $stockArray[5]."<br>";
+	echo $stockArray[5]."<br>";
 $sql6 = "SELECT DISTINCT stamp FROM Daily$stock WHERE ROUND(ltp,3) = '$closePrice'";
 $stamp1 = mysqli_query($con, $sql6);
 $row = mysqli_fetch_array($stamp1, MYSQL_ASSOC);
 $stamp=$row['stamp'];
 $stockArray[6] = substr($stamp,0,10);
-//	echo $stockArray[6]."<br>";
+	echo $stockArray[6]."<br>";
 	
-foreach ($stockArray as $value){
-	echo $value."<br>";
-}
-	insData($con, $stock, $stockArray);
+//foreach ($stockArray as $value){
+//	echo $value."<br>";
+//}
+//	insData($con, $stock, $stockArray);
 }
 function setIncrement($con, $stock){
 $sql = "SET @count = 0;";
