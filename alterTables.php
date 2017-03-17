642aade0aa5b662ba8dd359f90a05a97cce5319a
jaeznaer <jaeznaer11@gmail.com>
2017-03-17 09:49:16 +0530

    Add files via upload

---
+++ alterTables.php
@@ -0,0 +1,35 @@
+
+
+<?php
+$con=mysqli_connect("mysql.hostinger.in", "u659193285_jaez", "1994jitu", "u659193285_cldet") ;
+if (mysqli_connect_errno($con) ) {
+echo "Failed to connect to MySQL: " . mysqli_connect_error() ;
+}
+//echo "Connected to server successfully"."<br>";
+$result = mysqli_query($con,"SELECT nse_stock_name FROM NseStocks");
+$stockArray = Array();
+while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {
+    $stockArray[] =  $row['nse_stock_name'];  
+}
+
+foreach ($stockArray as $stock){
+	//setIncrement($con, $stock);
+	alterTable($con, $stock);
+}
+
+function alterTable($con, $stock){
+	//ALTER TABLE Person MODIFY P_Id INT(11) NOT NULL;
+$stockArray = Array();
+	
+$sql = "ALTER TABLE Daily$stock MODIFY ltp float NOT NULL;ALTER TABLE $stock MODIFY open float NOT NULL";
+
+
+if(mysqli_multi_query($con, $sql)){
+	echo "Vause inserted in Daily$stock";
+}else{
+	echo "Error running query";
+}
+
+mysqli_close($con) ;
+//echo "Connection to server closed successfully\r\n";
+?>
