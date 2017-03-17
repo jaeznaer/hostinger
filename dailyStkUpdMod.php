bef067bf1b8ce10012acc654524f9928a7353bd2
jaeznaer <jaeznaer11@gmail.com>
2017-03-17 15:41:05 +0530

    Update dailyStkUpdMod.php

--- dailyStkUpdMod.php
+++ dailyStkUpdMod.php
@@ -17,7 +17,7 @@ function addStockDetails($con, $stock){
 	$url = "http://finance.google.com/finance/info?client=ig&q=NSE:$stock";
 	//$json = file_get_contents("http://finance.google.com/finance/info?client=ig&q=NSE:$stock",NULL,NULL,4);
 	$curl_handle=curl_init();
-	curl_setopt($curl_handle, CURLOPT_URL,'http://###.##.##.##/mp/get?mpsrc=http://mybucket.s3.amazonaws.com/11111.mpg&mpaction=convert format=flv');
+	curl_setopt($curl_handle, CURLOPT_URL,$url);
 	curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
 	curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
 	//curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Your application name');
